dbb44dabe2a76d743c32029e71dee411dea4392c
PaigeAja <ajaknow@hotmail.com>
2018-06-25 23:02:48 -0400

    checkpoint-11 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,6 +13,7 @@
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('button#previous').on('click', function() {
@@ -25,4 +26,18 @@
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
   });
+
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') {return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
+
 }
